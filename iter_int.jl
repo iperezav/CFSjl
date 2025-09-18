9fd7f8aac7d8917ac7c90df43fde0e69e9306dba
Ivan Perez Avellaneda <iperezavellaneda@gmail.com>
2025-09-17 23:47:55 -0400

    the function to compute the iterative integrals is implemented

--- iter_int.jl
+++ iter_int.jl
@@ -19,28 +19,37 @@ Etemp = zeros(total_iterint, size(utemp, 2))
 
 ctrEtemp = zeros(Ntrunc+1)
 
-for i in 1:Ntrunc
-    ctrEtemp[i+1] =  ctrEtemp[i] + num_input^(i+1)
+for i in 0:Ntrunc-1
+    ctrEtemp[i+2] =  ctrEtemp[i+1] + num_input^(i+1)
 end
+
+#ctrEtemp[1] = 1
 
 sum_acc = cumsum(utemp, dims=2)*dt
 
 Etemp[1:num_input,begin:end] = hcat(zeros(num_input, 1), sum_acc[:,begin:end-1])
 
-for i in 1:Ntrunc
+for i in 1:Ntrunc-1
     
-    start_prev_block = 
-    utemp1 = repeat(utemp, inner=(2,1))
-
-    utemp2 = repeat(utemp, outer=(2,1))
-
-    utemp = utemp1.*utemp2
+    start_prev_block = Int(ctrEtemp[i])
+    end_prev_block = Int(ctrEtemp[i+1])
+    end_current_block = Int(ctrEtemp[i+2])
+    num_prev_block = end_prev_block - start_prev_block
+    num_current_block = end_current_block - end_prev_block
+    print("i: ", i, " start_prev_block: ", start_prev_block, " end_prev_block: ", end_prev_block, " end_current_block: ", end_current_block, " num_prev_block: ", num_prev_block, " num_current_block: ", num_current_block)
+    U_block = repeat(utemp, inner=(num_prev_block,1)) # inputs for current permutation
+    print("U_block size: ", size(U_block))
+    prev_int_block = repeat(Etemp[start_prev_block+1:end_prev_block,:], outer=(num_input,1)) # block of previous permutations
+    print("prev_int_block size: ", size(prev_int_block))
+    current_int_block = cumsum(U_block.*prev_int_block, dims = 2)*dt
+    print(size(current_int_block))
+    Etemp[end_prev_block+1:end_current_block,:] = hcat(zeros(num_current_block,1), current_int_block[:,begin:end-1])
 
 end
 
 
 
 
-u1.*u1
+
 
 
